28a6862279168cff09302baa7dd80c5d9897f3aa
Yokiijay <yokiijay@gmail.com>
2020-07-14 16:33:49 +0800

    *optimazition

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,40 @@
+import pkg from './package.json'
+import babel from '@rollup/plugin-babel'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
 
-export default {
-  input: {
+const extensions = ['.js','.jsx','.ts','.tsx']
 
-  },
-  output: {
-
-  },
-  plugins: {
-    
-  }
+/** @type {import('rollup').RollupOptions} */
+const options = {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs'
+    },
+    {
+      file: pkg.module,
+      format: 'esm',
+    }
+  ],
+  plugins: [
+    resolve({
+      extensions
+    }),
+    commonjs(),
+    babel({
+      babelHelpers: 'bundled',
+      extensions
+    }),
+    // terser(),
+  ],
+  external: [
+    'react',
+    '@emotion/core',
+    '@emotion/styled',
+    'styled-system'
+  ]
 }
+export default options
